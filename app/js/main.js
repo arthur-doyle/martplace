dccf737177ce9d0dff838180100d0cf9704350d2
arthur-doyle <anikolaus1995@gmail.com>
2020-09-11 01:20:33 +0300

    added section product-weekly, fix ratings in card tomorrow

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,20 @@
-$(function () {
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
 
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper__button-next',
+        prevEl: '.swiper__button-prev',
+    },
 });
+
+
+const ratingItemsList = querySelectorAll(".ratings__item");
+const ratingItemsArray = Array.prototype.slice.call(ratingItemsList);
+
+ratingItemsArray.forEach(item =>
+    item.addEventListener("click", () =>
+        item.parentNode.dataset.totalValue = item.dataset.itemValue)
+);
